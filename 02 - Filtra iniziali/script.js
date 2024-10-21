a6d61547bd8625c806acc32e5f361e204c35e795
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-21 15:05:59 +0200

    ADD: esercizio 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function startLetter (array,letter){
+    const newNames =[]
+    for(let i = 0; i < array.length; i++){
+        const element = array[i]
+        if (array[i].startsWith(letter)){
+            newNames.push(array[i])
+        }
+    }
+    return newNames
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
+startLetter(names,"A");
+console.log(startLetter(names,"A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
