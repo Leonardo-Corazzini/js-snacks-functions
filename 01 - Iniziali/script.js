8e90e10074d83b85f9146fff1a8ba40b5dd6bc23
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-21 14:50:56 +0200

    ADD: esercizio 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function initialLetters (array) {
+    const letters = []
+    for(let i = 0; i < array.length ; i++){
+        const element = array[i]
+        letters.push(element.charAt(0))
+    }
+    return letters
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
+initialLetters (names);
+console.log(initialLetters (names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
